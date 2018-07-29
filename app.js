ecbd1f1c534bed93c2edbcbca0dd5b606dba9079
john <johnmendozauni@gmail.com>
2018-07-28 22:18:56 -0500

    tasksApp

---
+++ app.js
@@ -0,0 +1,68 @@
+//const formTask = 
+document.getElementById('formTask').addEventListener('submit', saveTask);
+
+function saveTask(event) {
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+
+    const task = {
+        title, //title: title,
+        description //description: description 
+    };
+
+    if (localStorage.getItem('tasks') === null) {
+        let tasks = [];
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    } else {
+        let tasks = JSON.parse(localStorage.getItem('tasks'));
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+
+
+
+    getTask();
+    document.getElementById('formTask').reset();
+
+
+
+    event.preventDefault();
+}
+
+function getTask() {
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    let tasksView = document.getElementById('tasks');
+
+    tasksView.innerHTML = '';
+
+
+    for (let i = 0; i < tasks.length; i++) {
+        let title = tasks[i].title;
+        let description = tasks[i].description;
+        if (title.length == '' || description.length == '') {
+            continue;
+        } else {
+            tasksView.innerHTML += `
+        <div class="card mb-2">
+        <div class="card-body">
+        <p>${title} - ${description}</p>
+        <a class="btn btn-warning" onclick="deleteTask('${title}')">Delete</a>
+        </div>
+        </div>
+        `;
+        }
+    }
+}
+getTask();
+
+function deleteTask(title) {
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].title == title) {
+            tasks.splice(i, 1);
+        }
+    }
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    getTask();
+}
